f53f0e63d66f98695936acce6b7a619c62c35fc5
dzaima <dzaimagit@gmail.com>
2024-02-16 19:03:18 +0200

    quick-seek-ticks

--- src/DebuggerExtensionCommandHandler.cc
+++ src/DebuggerExtensionCommandHandler.cc
@@ -186,6 +186,11 @@ define hookpost-forward
 maintenance flush register-cache
 frame
 end
+
+define hookpost-quick-seek-ticks
+maintenance flush register-cache
+maintenance flush dcache
+end
 )Delimiter");
 
   return ss.str();
